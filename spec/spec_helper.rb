b4145ae0324d1e16858b3e32246d1760695cd8e3
Harald Skoglund <haraldsk@redpill-linpro.com>
2013-02-11 15:12:17 +0100

    Gemfile+Rakefile+Modified spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'mocha'
 require 'puppet'
 require 'rspec'
 require 'spec/autorun'
+require 'rubygems'
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 Spec::Runner.configure do |config|
     config.mock_with :mocha
