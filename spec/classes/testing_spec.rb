1ebc7a090c8d8e0a664844b3efff04b429843fd2
Harald Skoglund <haraldsk@redpill-linpro.com>
2013-02-11 16:11:01 +0100

    First tests.

--- spec/classes/testing_spec.rb
+++ spec/classes/testing_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
- 
+
+# http://rspec-puppet.com/tutorial/
+
 describe 'testing', :type => 'class' do
+
+  it { include_class('testing::params') }
+  it { contain_file('/etc/testing/conf.d/some_option.conf') }
 
 end
