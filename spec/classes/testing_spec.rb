3ee0bd4a0e4af1cb045999e55fa7b545e269d2be
Harald Skoglund <haraldsk@redpill-linpro.com>
2013-02-12 18:11:25 +0100

    Added a spec test.

--- spec/classes/testing_spec.rb
+++ spec/classes/testing_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
- 
+
 describe 'testing', :type => 'class' do
+  let(:facts) { {:osfamily => 'Debian' } }
+
+  it do
+    should include_class('testing::params')
+  end
 
 end
