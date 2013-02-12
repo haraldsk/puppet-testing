b7f2a69b7e0c244ba6bc19e8f44d92f01c43d77f
Harald Skoglund <haraldsk@redpill-linpro.com>
2013-02-12 14:42:47 +0100

    Bacported fix.

--- spec/defines/testing_option_spec.rb
+++ spec/defines/testing_option_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 describe 'testing::option', :type => 'define' do
   let(:title)  { 'some_option' }
-  let(:value)  { 'true'  } # h4x
   let(:params) { { :value => value } }
 
   it do
     should contain_file("/etc/testing/conf.d/#{title}.conf") \
     .with( { 'ensure' => 'present' } ) \
-    .with_content(/^\s*#{title}\s*=\s*#{value}\s*$/)
+    .with_content(/^\s*#{title}\s*=\s*#{params[:value]}\s*$/)
   end
   context 'ensure => absent' do
     let(:params) { { :ensure => 'absent' } }
