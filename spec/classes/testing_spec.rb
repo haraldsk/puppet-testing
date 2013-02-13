4749ea5f6374f7408789f80bade661d4cb1eb25b
Harald Skoglund <haraldsk@redpill-linpro.com>
2013-02-13 16:12:35 +0100

    Commented out stuff to begin with.

--- spec/classes/testing_spec.rb
+++ spec/classes/testing_spec.rb
@@ -23,50 +23,50 @@ describe 'testing', :type => 'class' do
       should contain_package('testing-suite')
     end
   end
-
-  context '$::osfamily = Windows' do
-    let(:facts) { {:osfamily => 'Windows' } }
-    it do
-       expect {
-        should contain_service('testing_service')
-       }.to raise_error(Puppet::Error, /Osfamily:\s*Windows/ )
-    end
-  end
-
-  context '$::osfamily = undef' do
-    let(:facts) { {  } }
-    it do
-       expect {
-        should contain_service('testing_service')
-       }.to raise_error(Puppet::Error, /Osfamily/ )
-    end
-  end
-
-  context 'include testing' do
-    it do
-      should contain_service('testing_service').with( {
-        'ensure'  => 'running',
-        'enable'  => 'true',
-      } )
-    end
-  end
-
-  context 'class { testing: service_ensure => foo }' do
-    let(:params) { {:service_ensure => 'foo' } }
-    it do
-       expect {
-        should contain_service('testing_service')
-       }.to raise_error(Puppet::Error, /validate_re.*foo/ )
-    end
-  end
-
-  context 'class { testing: service_enabled => false }' do
-    let(:params) { {:service_enable => 'false' } }
-    it do
-      should contain_service('testing_service').with( {
-        'enable' => 'false'
-      } )
-    end
-  end
+##
+#  context '$::osfamily = Windows' do
+#    let(:facts) { {:osfamily => 'Windows' } }
+#    it do
+#       expect {
+#        should contain_service('testing_service')
+#       }.to raise_error(Puppet::Error, /Osfamily:\s*Windows/ )
+#    end
+#  end
+#
+#  context '$::osfamily = undef' do
+#    let(:facts) { {  } }
+#    it do
+#       expect {
+#        should contain_service('testing_service')
+#       }.to raise_error(Puppet::Error, /Osfamily/ )
+#    end
+#  end
+## params
+#  context 'include testing' do
+#    it do
+#      should contain_service('testing_service').with( {
+#        'ensure'  => 'running',
+#        'enable'  => 'true',
+#      } )
+#    end
+#  end
+#
+#  context 'class { testing: service_ensure => foo }' do
+#    let(:params) { {:service_ensure => 'foo' } }
+#    it do
+#       expect {
+#        should contain_service('testing_service')
+#       }.to raise_error(Puppet::Error, /validate_re.*foo/ )
+#    end
+#  end
+#
+#  context 'class { testing: service_enabled => false }' do
+#    let(:params) { {:service_enable => 'false' } }
+#    it do
+#      should contain_service('testing_service').with( {
+#        'enable' => 'false'
+#      } )
+#    end
+#  end
 
 end
